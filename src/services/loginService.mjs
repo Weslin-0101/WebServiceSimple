979950e944f0d379f1b13c75a837ef12a1100350
Weslin-0101 <wesleeylirac12@gmail.com>
2022-02-03 08:44:40 -0300

    chore: finished the loginService

--- src/services/loginService.mjs
+++ src/services/loginService.mjs
@@ -1,5 +1,20 @@
+import { getUser } from "../data/dataContext.mjs";
+
 export class LoginService {
-    login() {
-        return { success: true };
+    login(data) {
+        const user = getUser(data.user);
+
+        if (!user || data.password !== user.password) {
+            return {
+                success: false,
+                message: "Credenciais erradas.",
+            };
+        }
+
+        return {
+            success: true,
+            message:
+                "Meu nome completo é Wesley Lira Carvalho. Eu quero essa vaga!",
+        };
     }
 }
