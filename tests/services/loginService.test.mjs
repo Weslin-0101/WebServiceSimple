a9340737f95d337363f23a6cc23417aa7bdd165a
Weslin-0101 <wesleeylirac12@gmail.com>
2022-02-03 12:39:22 -0300

    test: added the loginService test

--- tests/services/loginService.test.mjs
+++ tests/services/loginService.test.mjs
@@ -1,26 +1,31 @@
 import { LoginService } from "../../src/services/loginService.mjs";
+import jest from "jest-mock";
 
 describe("LoginService", () => {
     let sut;
     let sutRequest;
+    let validate;
     beforeEach(() => {
-        sut = new LoginService();
+        validate = {
+            execute: jest.fn(() => Promise.resolve({ success: true })),
+        };
+        sut = new LoginService(validate);
         sutRequest = {
             user: "syscoin",
             password: "meEscolhe",
         };
     });
 
-    it("deve ser logado com user como syscoin e senha como meEscolhe", () => {
-        const result = sut.login(sutRequest);
+    it("deve ser logado com user como syscoin e senha como meEscolhe", async () => {
+        const result = await sut.login(sutRequest);
 
         expect(result).toMatchObject({
             success: true,
         });
     });
 
-    it("deve aparecer a mensagem caso o login seja um sucesso", () => {
-        const result = sut.login(sutRequest);
+    it("deve aparecer a mensagem caso o login seja um sucesso", async () => {
+        const result = await sut.login(sutRequest);
 
         expect(result).toMatchObject({
             success: true,
@@ -32,8 +37,8 @@ describe("LoginService", () => {
     for (let i = 0; i < 3; i++) {
         const user = Math.random().toString(36).substring(6);
 
-        it(`o objeto success deve ser falso caso o usuário #${i} não seja syscoin`, () => {
-            const result = sut.login({ ...sutRequest, user });
+        it(`o objeto success deve ser falso caso o usuário #${i} não seja syscoin`, async () => {
+            const result = await sut.login({ ...sutRequest, user });
 
             expect(result).toMatchObject({
                 success: false,
@@ -44,8 +49,8 @@ describe("LoginService", () => {
     for (let i = 0; i < 3; i++) {
         const user = Math.random().toString(36).substring(6);
 
-        it(`o objeto success deve ser falso caso a senha do usuário #${i} não seja meEscolhe`, () => {
-            const result = sut.login({ ...sutRequest, user });
+        it(`o objeto success deve ser falso caso a senha do usuário #${i} não seja meEscolhe`, async () => {
+            const result = await sut.login({ ...sutRequest, user });
 
             expect(result).toMatchObject({
                 success: false,
@@ -53,9 +58,9 @@ describe("LoginService", () => {
         });
     }
 
-    it("deve aparecer uma mensagem se o usuário não estiver correto", () => {
+    it("deve aparecer uma mensagem se o usuário não estiver correto", async () => {
         const user = Math.random().toString(36).substring(7);
-        const result = sut.login({ ...sutRequest, user });
+        const result = await sut.login({ ...sutRequest, user });
 
         expect(result).toMatchObject({
             success: false,
@@ -63,13 +68,31 @@ describe("LoginService", () => {
         });
     });
 
-    it("deve aparecer uma mensagem se a senha não estiver correta", () => {
+    it("deve aparecer uma mensagem se a senha não estiver correta", async () => {
         const user = Math.random().toString(36).substring(6);
-        const result = sut.login({ ...sutRequest, user });
+        const result = await sut.login({ ...sutRequest, user });
 
         expect(result).toMatchObject({
             success: false,
             message: "Credenciais erradas.",
+        });
+    });
+
+    it("deve chama o validator", async () => {
+        await sut.login(sutRequest);
+
+        expect(validate.execute.mock.calls.length).toBe(1);
+    });
+
+    it("deve chama o validator caso o success seja falso", async () => {
+        validate.execute = jest.fn(() =>
+            Promise.resolve({ success: false, flag: true })
+        );
+        const resul = await sut.login(sutRequest);
+
+        expect(resul).toMatchObject({
+            success: false,
+            flag: true,
         });
     });
 });
