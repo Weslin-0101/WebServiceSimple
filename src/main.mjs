589f6384bb10023d16164adf2db8359f3896d491
Weslin-0101 <wesleeylirac12@gmail.com>
2022-02-03 13:21:22 -0300

    feat: added the Route in the code

--- src/main.mjs
+++ src/main.mjs
@@ -1,13 +1,19 @@
 import "dotenv/config";
 import express from "express";
+import { route } from "./routes.mjs";
 
 const app = express();
 app.use(express.json());
+app.use(route);
 
 app.get("/", (req, res) => {
     res.status(200).json({
         message: "AOBAAAAA",
     });
+});
+
+app.get("*", (req, res) => {
+    return res.status(404).json({ message: "Página não encontrada" });
 });
 
 const port = process.env.PORT || 3000;
