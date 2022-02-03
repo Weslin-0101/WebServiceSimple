95e2e9d66f4f7e43ef0994d83aae622fbac5fc0c
Weslin-0101 <wesleeylirac12@gmail.com>
2022-02-03 12:36:53 -0300

    chore: update the main

--- src/main.mjs
+++ src/main.mjs
@@ -1,3 +1,16 @@
-export function Sum(a, b) {
-    return a + b;
-}
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "AOBAAAAA",
+    });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`"Backend fluindo na porta ${port}...`);
+});
